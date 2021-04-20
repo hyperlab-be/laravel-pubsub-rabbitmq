8ccb956a65c50a00c8f3733ff3bab4d9b89d348c
Michiel Kempen <kempenmichiel@gmail.com>
2021-04-20 14:56:49 +0200

    add reconnection logic to register command

--- src/Subscriber/Commands/Register.php
+++ src/Subscriber/Commands/Register.php
@@ -5,12 +5,16 @@ namespace Hyperlab\LaravelPubSubRabbitMQ\Subscriber\Commands;
 use Hyperlab\LaravelPubSubRabbitMQ\PubSubConnector;
 use Hyperlab\LaravelPubSubRabbitMQ\Subscriber\Subscriptions;
 use Illuminate\Console\Command;
+use PhpAmqpLib\Exception\AMQPIOException;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue;
 
 class Register extends Command
 {
-    public $signature = 'pubsub:register';
+    public $signature = '
+        pubsub:register
+        {--timeout=60 : Time in seconds that connecting should be attempted}
+    ';
 
     public $description = 'Register the consumer with RabbitMQ.';
 
@@ -19,12 +23,51 @@ class Register extends Command
         $exchange = config('pubsub.rabbitmq.exchange');
         $queue = config('pubsub.rabbitmq.queue');
 
-        $config = config('queue.connections.'.config('pubsub.queue.connection'));
-        $connection = PubSubConnector::new()->connect($config);
-
+        $connection = $this->connectToRabbitMQ();
         $this->declareExchange($connection, $exchange);
         $this->declareQueue($connection, $queue);
         $this->declareQueueBindings($connection, $exchange, $queue);
+    }
+
+    private function connectToRabbitMQ(): RabbitMQQueue
+    {
+        $config = config('queue.connections.'.config('pubsub.queue.connection'));
+        $connector = PubSubConnector::new();
+
+        $this->info('Waiting for a successful connection...');
+
+        $timeout = $this->getTimeout();
+        $this->output->progressStart($timeout);
+
+        do {
+            try {
+                $connection = $connector->connect($config);
+            } catch (AMQPIOException $e) {
+                if($e->getCode() !== 111 || $timeout <= 0) {
+                    throw $e;
+                }
+                $timeout--;
+                sleep(1);
+                $this->output->progressAdvance();
+            }
+        } while (! isset($connection));
+
+        $this->output->progressFinish();
+
+        $this->info('Successfully established a connection.');
+
+        return $connection;
+    }
+
+    private function getTimeout(): int
+    {
+        $timeout = $this->option('timeout') ?? 60;
+
+        if (! is_numeric($timeout)) {
+            throw new \InvalidArgumentException('Must pass an integer to option: timeout');
+        }
+
+        return (int) $timeout;
     }
 
     private function declareExchange(RabbitMQQueue $connection, string $exchange): void
